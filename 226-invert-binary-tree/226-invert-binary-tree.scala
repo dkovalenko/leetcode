1723731b1cd8ec6e10c748c851fc6549ac1335ec
Dmitriy Kovalenko <dimon.theonly@gmail.com>
2022-09-24 23:52:18 +0300

    Time: 819 ms (38.54%), Space: 68.7 MB (58.33%) - LeetHub

---
+++ 226-invert-binary-tree/226-invert-binary-tree.scala
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
+ *   var value: Int = _value
+ *   var left: TreeNode = _left
+ *   var right: TreeNode = _right
+ * }
+ */
+object Solution {
+    def invertTree(root: TreeNode): TreeNode = {
+      if (root == null) return null
+      val newRight = invertTree(root.left)
+      val newLeft = invertTree(root.right)
+      root.left = newLeft
+      root.right = newRight
+      root
+    }
+}
