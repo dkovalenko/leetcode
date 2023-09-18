fd12d707d1b5b782e63a1059cd9bf61c58ae103c
Dmytro Kovalenko <dimon.theonly@gmail.com>
2023-09-18 23:35:42 +0200

    Time: 493 ms (22.73%), Space: 57.1 MB (31.82%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.scala
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.scala
@@ -1,3 +1,4 @@
+import collection.mutable._
 /**
  * Definition for a binary tree node.
  * class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
@@ -7,12 +8,30 @@
  * }
  */
 object Solution {
+  val stack = Stack.empty[TreeNode]
+  
   def preorderTraversal(root: TreeNode): List[Int] = {
-    dfs(root)
+    //dfs(root)
+    val result = ListBuffer.empty[Int]
+    stack.push(root)
+    iterative(result)
+    result.toList
   }
   
   def dfs(node: TreeNode): List[Int] = {
     if (node == null) return Nil
     List(node.value) ++ dfs(node.left) ++ dfs(node.right)
+  }
+  
+  def iterative(result: ListBuffer[Int]): Unit = {
+    while (stack.nonEmpty) {
+      var currentNode = stack.pop
+      if (currentNode != null) {
+        result += currentNode.value
+        stack.push(currentNode.right)
+        stack.push(currentNode.left)
+      }
+    }
+    ()
   }
 }
