6bcef548daa8a564d3c0b0e9cea9beddf930ec1d
Dmytro Kovalenko <dimon.theonly@gmail.com>
2023-09-19 22:34:03 +0200

    Time: 529 ms (82.05%), Space: 57.6 MB (92.31%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.scala
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.scala
@@ -1,3 +1,4 @@
+import scala.collection.mutable._
 /**
  * Definition for a binary tree node.
  * class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
@@ -10,23 +11,41 @@ object Solution {
   def levelOrder(root: TreeNode): List[List[Int]] = {
     val q = collection.mutable.Queue.empty[TreeNode]
     val buf = collection.mutable.ArrayBuffer.empty[List[Int]]
+    val bufRecur = collection.mutable.Map.empty[Int, ListBuffer[Int]]
 
-    if (root == null) return Nil
-    q.enqueue(root)
-    while(q.nonEmpty) {
-      val currentElements = q.dequeueAll(_ => true)
-      buf += currentElements.map(_.value).toList
-      currentElements.foreach { currentEl =>
+    // if (root == null) return Nil
+    // q.enqueue(root)
+    // while(q.nonEmpty) {
+    //   val currentElements = q.dequeueAll(_ => true)
+    //   buf += currentElements.map(_.value).toList
+    //   currentElements.foreach { currentEl =>
         
-        if (currentEl.left != null) {
-          q.enqueue(currentEl.left)
-        }
-        if (currentEl.right != null) {
-          q.enqueue(currentEl.right)
-        }
+    //     if (currentEl.left != null) {
+    //       q.enqueue(currentEl.left)
+    //     }
+    //     if (currentEl.right != null) {
+    //       q.enqueue(currentEl.right)
+    //     }
+    //   }
+    // }
+    def levelOrderRecur(node: TreeNode, level: Int): Unit = {
+      if (node == null) return ()
+      bufRecur.get(level) match {
+        case Some(lb) => lb += node.value
+        case None => bufRecur.put(level, ListBuffer(node.value))
+      } 
+      if (node.left != null) {
+        levelOrderRecur(node.left, level + 1)
+      }
+      if (node.right != null) {
+        levelOrderRecur(node.right, level + 1)
       }
     }
+    levelOrderRecur(root, 0)
 
-    buf.toList
+    bufRecur.values.map(_.toList).toList
+    // buf.toList
   }
+
+  
 }
