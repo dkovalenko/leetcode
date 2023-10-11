60267cb39e5532f7ef06876dcacb392259ebab3e
Dmytro Kovalenko <dimon.theonly@gmail.com>
2023-10-11 22:34:01 +0200

    Time: 555 ms (80.31%), Space: 59.3 MB (70.87%) - LeetHub

--- 0169-majority-element/0169-majority-element.scala
+++ 0169-majority-element/0169-majority-element.scala
@@ -1,5 +1,21 @@
 object Solution {
   def majorityElement(nums: Array[Int]): Int = {
+    var candidate: Int = -1
+    var counter = 0
+    nums.foreach { num =>
+      if (candidate ==  -1 || counter == 0) {
+        candidate = num
+      } 
+      if (candidate == num) {
+        counter += 1
+      } else {
+        counter -= 1
+      }
+    }
+    candidate
+  }
+
+  def majorityElementFreq(nums: Array[Int]): Int = {
     val freq = collection.mutable.Map.empty[Int, Int]
     if (nums.size == 1) return nums(0)
     nums.foreach { num =>
